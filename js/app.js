c3d1b69ae83bb21645e759dcba7d4c509e3e4cfe
jimmy-oss <jimmynjonge51@gmail.com>
2021-11-30 15:46:58 +0300

    main files

---
+++ js/app.js
@@ -0,0 +1,87 @@
+const auth = "563492ad6f9170000100000114b84eb6bdfe4a4ebf1b48ca797196ef";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".search-form");
+const more = document.querySelector(".more");
+let searchValue;
+let page = 1;
+let fetchLink;
+let currentSearch;
+
+// Event Listeners
+searchInput.addEventListener("input", updateInput);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  currentSearch = searchValue; // store search value inside current search before it's cleared
+  searchPhotos(searchValue);
+});
+
+more.addEventListener("click", loadMore);
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
+  const data = await dataFetch.json();
+  return data;
+}
+
+function generatePictures(data) {
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    console.log(photo);
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `
+    <div class="gallery-info">
+    <p>${photo.photographer}</p>
+    <a href="${photo.src.original}">Download</a>
+    </div>
+    <img src="${photo.src.large}"></img>
+    `;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function curatedPhotos() {
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
+}
+
+// https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1
+async function searchPhotos(query) {
+  clear();
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
+  console.log(data);
+  generatePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
+  page = 1;
+}
+
+async function loadMore() {
+  // Needs to load more of popular images (home) and more search query images
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
+}
+
+curatedPhotos();
